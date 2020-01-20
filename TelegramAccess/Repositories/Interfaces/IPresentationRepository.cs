729b7f27fdfb548eb2ec5fdac025b4e655e46cb5
maxvonlancaster <windchange7@gmail.com>
2020-01-20 18:17:59 +0200

    sdfn

--- TelegramAccess/Repositories/Interfaces/IPresentationRepository.cs
+++ TelegramAccess/Repositories/Interfaces/IPresentationRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TelegramAccess.Entities;
 
 namespace TelegramAccess.Repositories.Interfaces
 {
     public interface IPresentationRepository
     {
+        void Create(Presentation presentation);
+        void Edit(int id, Presentation presentation);
+        void Delete(int id);
+        Presentation Get(int id);
+        List<Presentation> GetAll();
     }
 }
--- TelegramAccess/Repositories/PresentationRepository.cs
+++ TelegramAccess/Repositories/PresentationRepository.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TelegramAccess.Entities;
 using TelegramAccess.Repositories.Interfaces;
 
 namespace TelegramAccess.Repositories
 {
     public class PresentationRepository : IPresentationRepository
     {
+        public void Create(Presentation presentation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id, Presentation presentation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Presentation Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Presentation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
