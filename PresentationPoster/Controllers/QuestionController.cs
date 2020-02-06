0443aa64a0d40d39ff4cb0e87cb307fc3c4ace17
windchange7 <windchange7@gmail.com>
2020-02-06 08:38:27 +0200

    question controller added

--- PresentationPoster/Controllers/QuestionController.cs
+++ PresentationPoster/Controllers/QuestionController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TelegramAccess.Entities;
+using TelegramAccess.Repositories.Interfaces;
 
 namespace PresentationPoster.Controllers
 {
@@ -11,5 +13,37 @@ namespace PresentationPoster.Controllers
     [ApiController]
     public class QuestionController : ControllerBase
     {
+        private readonly IQuestionRepository _questionRepository;
+
+        public QuestionController(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        [HttpGet]
+        [Route("getQuestions")]
+        public async Task<IActionResult> GetQuestions() 
+        {
+            var questions = await _questionRepository.GetAll();
+            return new JsonResult(questions);
+        }
+
+        [HttpGet]
+        [Route("getQuestion")]
+        public async Task<IActionResult> GetQuestion([FromBody] int i) 
+        {
+            var question = await _questionRepository.Get(i);
+            return new JsonResult(question);
+        }
+
+        [HttpPost]
+        [Route("postQuestion")]
+        public async Task<IActionResult> PostQuestion([FromBody] Question question) 
+        {
+            await _questionRepository.Create(question);
+            return new JsonResult(true);
+        }
+
+
     }
 }
